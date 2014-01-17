f51cf64173539fa43581c8e110b0a8d3d6fb1301
Damien Doury <damien.doury@hotmail.fr>
2014-01-17 14:53:12 +0100

    Cursor management

---
+++ AnimationTester/Assets/Scripts/ManageCursor.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManageCursor : MonoBehaviour
+{
+	private SelectedMove selectedMove;
+	private GameObject hoverEntity;
+
+	private Texture2D hand;
+	private Vector2 handPos;
+	private Texture2D single;
+	private Vector2 singlePos;
+	private Texture2D singleAlly;
+	private Vector2 singleAllyPos;
+	private Texture2D singleEnemy;
+	private Vector2 singleEnemyPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PokemonController controller = GameObject.FindGameObjectWithTag("CameraTarget").GetComponent<PokemonController>();
+		selectedMove = controller.selectedMove;
+		hoverEntity = controller.hoverEntity;
+
+		hand = (Texture2D)Resources.Load("GUI/Cursor/Hand", typeof(Texture2D));
+		single = (Texture2D)Resources.Load("GUI/Cursor/SingleTarget", typeof(Texture2D));
+		singleAlly = (Texture2D)Resources.Load("GUI/Cursor/SingleTarget_Ally", typeof(Texture2D));
+		singleEnemy = (Texture2D)Resources.Load("GUI/Cursor/SingleTarget_Enemy", typeof(Texture2D));
+		
+		handPos = new Vector2(9f, 12f);
+		singlePos = new Vector2(24f, 24f);
+		singleAllyPos = new Vector2(24f, 24f);
+		singleEnemyPos = new Vector2(24f, 24f);
+	}
+	
+	void Update()
+	{
+		if(!(selectedMove != null && !selectedMove.IsLaunched()))
+		{
+			Cursor.SetCursor(hand, handPos, CursorMode.ForceSoftware);
+		}
+		else if(selectedMove != null && !selectedMove.IsLaunched())
+		{
+			TargetType targetType = selectedMove.info.targetType;
+			
+			if(targetType == TargetType.Area)
+			{
+				Cursor.SetCursor(singleEnemy, singleEnemyPos, CursorMode.ForceSoftware);
+			}
+			else
+			{
+				if(hoverEntity)
+				{
+					if(targetType == TargetType.Self && hoverEntity.Equals(gameObject))
+					{
+						Cursor.SetCursor(singleAlly, singleAllyPos, CursorMode.ForceSoftware);
+					}
+					else if(targetType == TargetType.Enemy && !hoverEntity.Equals(gameObject))
+					{
+						Cursor.SetCursor(singleEnemy, singleEnemyPos, CursorMode.ForceSoftware);
+					}
+					else
+					{
+						Cursor.SetCursor(single, singlePos, CursorMode.ForceSoftware);
+					}
+				}
+				else
+				{
+					Cursor.SetCursor(single, singlePos, CursorMode.ForceSoftware);
+				}
+			}
+		}
+	}
+}
